1d536c22d5211dd0df057b35d3187fc075415d83
Andrenunes95 <lucas.a.nunes12@gmail.com>
2026-03-17 15:24:47 -0300

    :tada: init: Primeiro commit realizado.

---
+++ AtividadeAula02.js
@@ -0,0 +1,90 @@
+// ATIVIDADE AULA 02 - LUCAS ANDRÉ
+
+// EXERCÍCIO 01
+
+let nome ="André"
+
+const idade = 30;
+
+const profissão = "Programador"
+
+const Cidade = "São Paulo"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade, sou ${profissão} e moro em ${Cidade}`)
+
+// EXERCÍCIO 02
+
+let Smartphone = "IPhone"
+let preço = 2000.00
+let quantidade = 5
+
+console.log(`Comprei ${quantidade} unidades de ${Smartphone} por R$ ${preço} cada`)
+
+
+// EXERCÍCIO 03 
+
+let valor1 = 10
+let valor2 = 20
+
+
+console.log (valor1+valor2);
+console.log (valor1-valor2);
+console.log (valor1*valor2);
+console.log (valor1/valor2);
+
+
+// EXERCÍCIO 04
+
+let valor = 200
+console.log (typeof valor);
+let texto = String (valor);
+console.log(typeof texto);
+
+
+
+let hojeEuVenci = true;
+let hojeEuNãoVenci = false;
+
+console.log (typeof hojeEuVenci);
+
+
+// EXERCÍCIO 05
+
+let número = 200
+console.log (typeof número);
+let linhas = String (número);
+console.log(typeof linhas);
+
+
+// DESAFIO FINAL
+
+
+
+console.log(Cabeçalho);
+nome = "Cadastro do Usuário";
+console.log(Cabeçalho);
+
+console.log(Nome);
+nome = "Lucas";
+console.log(Nome);
+
+console.log(Idade);
+nome = 31;
+console.log(Idade);
+
+console.log(Altura);
+nome = 1.75;
+console.log(Altura);
+
+console.log(Peso);
+nome = 75;
+console.log(Peso);
+
+console.log(Localidade);
+nome = "São Paulo";
+console.log(Localidade);
+
+console.log(Ocupação);
+nome = "Programador";
+console.log(Ocupação);
+
---
+++ script.js
@@ -0,0 +1,187 @@
+/*
+
+const idade = 30;
+let nome = "André";
+
+var cidade = "São Caetano do Sul";
+/*
+console.log(idade);
+console.log(nome);
+console.log(cidade);
+
+console.log(nome);
+nome = "Lucas";
+console.log(nome);
+
+console.log(idade);
+idade=25;
+console.log(idade);
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade.`);
+console.log("Juntando nome"  + nome +  "com idade"  + idade)
+
+
+let mensagem = "Olá Mundo!";
+console.log(typeof mensagem);
+
+let quantidade = 30;
+let temperatura = 36.5;
+
+console.log (typeof quantidade)
+
+
+let estaChovendo = true;
+let temSol = false;
+
+console.log (typeof temSol)
+
+let pedido;
+console.log(typeof pedido);
+
+let endereço = null;
+console.log (typeof endereço);
+
+
+console.log("5"  + 3);
+console.log("5" - 3 );
+
+let numero = "42";
+console.log(typeof numero);
+let numeroConvertido = Number(numero);
+console.log(typeof numeroConvertido);
+
+let num = 100;
+console.log (typeof num);
+let texto = String (num);
+console.log(typeof texto);
+
+let trueConvertido = Number (true);
+console.log(trueConvertido);
+
+let falseConvertido = Number (false);
+
+console.log(falseConvertido);
+
+
+
+console.log(10>5);
+console.log(10<5);
+
+console.log(5=="5");
+console.log(5==="5");
+
+
+
+let a = 10;
+let b = 5;
+
+console.log (a+b);
+console.log (a-b);
+console.log (a*b);
+console.log (a/b);
+
+console.log(10 % 3);
+console.log(2**3);
+*/
+
+// ATIVIDADE AULA 02 - LUCAS ANDRÉ
+
+// EXERCÍCIO 01
+
+let nome ="André"
+
+const idade = 30;
+
+const profissão = "Programador"
+
+const Cidade = "São Paulo"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos de idade, sou ${profissão} e moro em ${Cidade}`)
+
+// EXERCÍCIO 02
+
+let Smartphone = "IPhone"
+let preço = 2000.00
+let quantidade = 5
+
+console.log(`Comprei ${quantidade} unidades de ${Smartphone} por R$ ${preço} cada`)
+
+
+// EXERCÍCIO 03 
+
+let valor1 = 10
+let valor2 = 20
+
+
+console.log (valor1+valor2);
+console.log (valor1-valor2);
+console.log (valor1*valor2);
+console.log (valor1/valor2);
+
+
+// EXERCÍCIO 04
+
+let valor = 200
+console.log (typeof valor);
+let texto = String (valor);
+console.log(typeof texto);
+
+
+
+let hojeEuVenci = true;
+let hojeEuNãoVenci = false;
+
+console.log (typeof hojeEuVenci);
+
+
+// EXERCÍCIO 05
+
+let número = 200
+console.log (typeof número);
+let linhas = String (número);
+console.log(typeof linhas);
+
+
+// DESAFIO FINAL
+
+let Cabeçalho = "Cadastro do Usuário:"
+
+let Nome = "Lucas";
+
+const Idade = 31;
+
+let Altura = 1.75;
+
+let Peso = 75;
+
+let Localidade = "São Paulo"
+
+let Ocupação = "Programador"
+
+console.log(Cabeçalho);
+nome = "Cadastro do Usuário";
+console.log(Cabeçalho);
+
+console.log(Nome);
+nome = "Lucas";
+console.log(Nome);
+
+console.log(Idade);
+nome = 31;
+console.log(Idade);
+
+console.log(Altura);
+nome = 1.75;
+console.log(Altura);
+
+console.log(Peso);
+nome = 75;
+console.log(Peso);
+
+console.log(Localidade);
+nome = "São Paulo";
+console.log(Localidade);
+
+console.log(Ocupação);
+nome = "Programador";
+console.log(Ocupação);
